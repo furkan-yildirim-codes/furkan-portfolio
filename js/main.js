00335230353249aae306638312069ea89a04d437
Furkan Yildirim <fyildirim8@stu.okan.edu.tr>
2026-02-21 12:30:52 +0300

    A hamburger was organized

--- js/main.js
+++ js/main.js
@@ -3,9 +3,17 @@ const navLinks = document.getElementById("navLinks");
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+    hamburger.classList.toggle("active");
 });
 
 window.addEventListener("scroll", function() {
     const navbar = this.document.querySelector(".navbar");
     navbar.classList.toggle("scrolled", this.window.scrollY > 50);
 });
+
+document.addEventListener("click", (e) => {
+    if (!hamburger.contains(e.target) && !navLinks.contains(e.target)) {
+        navLinks.classList.remove("active");
+        hamburger.classList.remove("active");
+    }
+})
