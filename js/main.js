f6fd3f95596f3ce6f9033f83941ede31f5cf06fc
Furkan Yildirim <fyildirim8@stu.okan.edu.tr>
2026-02-21 12:09:25 +0300

    Adjustments have been made

--- js/main.js
+++ js/main.js
@@ -4,3 +4,8 @@ const navLinks = document.getElementById("navLinks");
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+window.addEventListener("scroll", function() {
+    const navbar = this.document.querySelector(".navbar");
+    navbar.classList.toggle("scrolled", this.window.scrollY > 50);
+});
