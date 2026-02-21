90e217453230cd917d764bfb1d513feec64ee81b
Furkan Yildirim <fyildirim8@stu.okan.edu.tr>
2026-02-21 17:26:34 +0300

    Updated scroll

--- js/main.js
+++ js/main.js
@@ -17,3 +17,18 @@ document.addEventListener("click", (e) => {
         hamburger.classList.remove("active");
     }
 })
+
+const reveals = document.querySelectorAll(".reveal");
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("active");
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+reveals.forEach(reveal => {
+    observer.observe(reveal);
+});
