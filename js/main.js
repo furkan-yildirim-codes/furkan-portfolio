38e3d14bf26f75aab656c4c1a6db1463b6b0b104
Furkan Yildirim <fyildirim8@stu.okan.edu.tr>
2026-02-27 20:57:08 +0300

    Added down to up button

--- js/main.js
+++ js/main.js
@@ -93,3 +93,20 @@ themeToggle.addEventListener("click", () => {
 themeToggle.addEventListener("click", () => {
     themeToggle.blur();
 });
+
+const scrollTopBtn = document.querySelector(".scroll-top-btn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add("show");
+  } else {
+    scrollTopBtn.classList.remove("show");
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
